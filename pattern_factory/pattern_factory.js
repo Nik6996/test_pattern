b7e9c7bab6fc2bdb784bcd17014687c9b24077b9
Nik6996 <nik.taraskov@ukr.net>
2021-08-21 03:15:26 +0400

    finish factory

--- pattern_factory/pattern_factory.js
+++ pattern_factory/pattern_factory.js
@@ -6,31 +6,21 @@ var BmwCar = /** @class */ (function () {
     }
     return BmwCar;
 }());
-var VolvoCar = /** @class */ (function () {
-    function VolvoCar(model, price, autoPilot) {
-        this.model = model;
-        this.price = price;
-        this.autoPilot = autoPilot;
-    }
-    return VolvoCar;
-}());
 var CarFactory = /** @class */ (function () {
     function CarFactory() {
     }
     CarFactory.prototype.create = function (type) {
         if (type === 'X3')
             return new BmwCar(type, 5000, 200);
-        if (type === 'W8')
-            return new VolvoCar(type, 10000, true);
-        if (type === 'X6')
+        if (type === 'X5')
             return new BmwCar(type, 20000, 350);
     };
     return CarFactory;
 }());
 var newBmw = new CarFactory();
-var x5 = newBmw.create('X6');
+var x5 = newBmw.create('X5');
 console.log(x5);
-var newVolvo = new CarFactory();
-var w8 = newVolvo.create('W8');
-console.log(w8);
+var newBmw1 = new CarFactory();
+var x3 = newBmw.create('X3');
+console.log(x3);
 //# sourceMappingURL=pattern_factory.js.map
--- pattern_factory/pattern_factory.ts
+++ pattern_factory/pattern_factory.ts
@@ -4,11 +4,6 @@ interface TopCar {
 	price: number
 	speed: number
 }
-interface TopCar1 {
-	model: string | number
-	price: number
-	autoPilot: boolean
-}
 
 
 class BmwCar implements TopCar {
@@ -18,32 +13,23 @@ class BmwCar implements TopCar {
 		public speed: number) { }
 }
 
-class VolvoCar implements TopCar1 {
-	constructor(
-		public model: string | number,
-		public price: number,
-		public autoPilot: boolean
-	) { }
-}
 
 class CarFactory {
-	create(type: string) {
+	create(type: string): TopCar {
 		if (type === 'X3')
 			return new BmwCar(type, 5000, 200);
-		if (type === 'W8')
-			return new VolvoCar(type, 10000, true);
-		if (type === 'X6')
+		if (type === 'X5')
 			return new BmwCar(type, 20000, 350);
 	}
 }
 
 const newBmw = new CarFactory();
-let x5 = newBmw.create('X6');
+let x5 = newBmw.create('X5');
 console.log(x5);
 
+const newBmw1 = new CarFactory();
+let x3 = newBmw.create('X3');
+console.log(x3);
 
-const newVolvo = new CarFactory()
-let w8 = newVolvo.create('W8')
-console.log(w8)
 
 
