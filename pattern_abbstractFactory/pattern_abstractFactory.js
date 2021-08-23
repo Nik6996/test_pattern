188968ed5617d5b5399b9f0ac7ae49c932c8f7f8
Nik6996 <nik.taraskov@ukr.net>
2021-08-24 03:13:27 +0400

    finish

--- pattern_abbstractFactory/pattern_abstractFactory.js
+++ pattern_abbstractFactory/pattern_abstractFactory.js
@@ -1,49 +1,105 @@
-var ProducerCar = /** @class */ (function () {
-    function ProducerCar() {
+// конкретный продукт
+var AudiProductDiesel = /** @class */ (function () {
+    function AudiProductDiesel() {
+        this.price = 2000;
+        this.model = "Audi S6";
     }
-    ProducerCar.prototype.create = function (typeCar) {
-        if (typeCar === 'family')
-            return new FamilyCar().bmwX5;
-        if (typeCar === 'sport')
-            return new SportCar().bmwM4;
+    AudiProductDiesel.prototype.info = function () {
+        return "M\u043E\u0434\u0435\u043B\u044C \u0430\u0432\u0442\u043E " + this.model + " \u0435\u0433\u043E \u0446\u0435\u043D\u0430 " + this.price + "$";
     };
-    return ProducerCar;
+    return AudiProductDiesel;
 }());
-var SportCar = /** @class */ (function () {
-    function SportCar() {
+var AudiProductElectric = /** @class */ (function () {
+    function AudiProductElectric() {
     }
-    SportCar.prototype.bmwM4 = function () {
-        return new BmwM4();
+    AudiProductElectric.prototype.model = function () {
+        return "Audi A6";
     };
-    return SportCar;
+    return AudiProductElectric;
 }());
-var FamilyCar = /** @class */ (function () {
-    function FamilyCar() {
+var BmwProductDiesel = /** @class */ (function () {
+    function BmwProductDiesel() {
+        this.price = 3000;
+        this.model = "Bmw M5";
     }
-    FamilyCar.prototype.bmwX5 = function () {
-        return new BmwX5();
+    BmwProductDiesel.prototype.info = function () {
+        return "M\u043E\u0434\u0435\u043B\u044C \u0430\u0432\u0442\u043E " + this.model + " \u0435\u0433\u043E \u0446\u0435\u043D\u0430 " + this.price + "$";
     };
-    return FamilyCar;
+    return BmwProductDiesel;
 }());
-var BmwM4 = /** @class */ (function () {
-    function BmwM4() {
+var BmwProductElectric = /** @class */ (function () {
+    function BmwProductElectric() {
     }
-    BmwM4.prototype.info = function () {
-        return 'Bmw sport car';
+    BmwProductElectric.prototype.model = function () {
+        return "Bmw I3";
     };
-    return BmwM4;
+    return BmwProductElectric;
 }());
-var BmwX5 = /** @class */ (function () {
-    function BmwX5() {
+var FactoryAudiCar = /** @class */ (function () {
+    function FactoryAudiCar() {
     }
-    BmwX5.prototype.info = function () {
-        return 'Bmw family car';
+    FactoryAudiCar.prototype.createDieselCar = function () {
+        return new AudiProductDiesel();
     };
-    return BmwX5;
+    FactoryAudiCar.prototype.createElectroCar = function () {
+        return new AudiProductElectric();
+    };
+    return FactoryAudiCar;
 }());
-var carTest1 = new ProducerCar();
-var carTest2 = carTest1.create('sport');
-var carTest3 = carTest1.create('family');
-console.log(carTest2);
-console.log(carTest3);
+var FactoryBmwCar = /** @class */ (function () {
+    function FactoryBmwCar() {
+    }
+    FactoryBmwCar.prototype.createDieselCar = function () {
+        return new BmwProductDiesel();
+    };
+    FactoryBmwCar.prototype.createElectroCar = function () {
+        return new BmwProductElectric();
+    };
+    return FactoryBmwCar;
+}());
+function code(factory) {
+    var audi = factory.createDieselCar();
+    var bmw = factory.createElectroCar();
+    console.log(audi.info());
+    console.log(bmw.model());
+}
+code(new FactoryBmwCar());
+code(new FactoryAudiCar());
+// type CarsType = string | number;
+// interface Producer {
+// 	create(typeCar: CarsType): CarsType
+// }
+// class ProducerCar implements Producer {          // абстрактная фабрика
+// 	create(typeCar: CarsType): CarsType {
+// 		if (typeCar === 'family')
+// 			return new FamilyCar().bmwX5()
+// 		if (typeCar === 'sport')
+// 			return new SportCar().bmwM4()
+// 	}
+// }
+// class SportCar {
+// 	bmwM4(): CarsType {
+// 		return new BmwM4().info();
+// 	}
+// }
+// class FamilyCar {
+// 	bmwX5(): CarsType {
+// 		return new BmwX5().info();
+// 	}
+// }
+// class BmwM4 {
+// 	info(): CarsType {
+// 		return 'Bmw sport car'
+// 	}
+// }
+// class BmwX5 {
+// 	info(): CarsType {
+// 		return 'Bmw family car'
+// 	}
+// }
+// let carTest1 = new ProducerCar()
+// let carTest2 = carTest1.create('sport');
+// let carTest3 = carTest1.create('family');
+// console.log(carTest2)
+// console.log(carTest3)
 //# sourceMappingURL=pattern_abstractFactory.js.map
--- pattern_abbstractFactory/pattern_abstractFactory.ts
+++ pattern_abbstractFactory/pattern_abstractFactory.ts
@@ -1,43 +1,135 @@
+// конкретный продукт
 
+interface ProductDieselCar {
+	info(): string | number
+	price: number
+	model: string
+}
+interface ProductElectroCar {
+	model(): string
+}
 
-class ProducerCar {
-	create(typeCar: string) {
-		if (typeCar === 'family')
-			return new FamilyCar().bmwX5
-		if (typeCar === 'sport')
-			return new SportCar().bmwM4
+class AudiProductDiesel implements ProductDieselCar {
+	price: number = 2000
+	model: string = "Audi S6"
+	public info(): string | number {
+		return `Mодель авто ${this.model} его цена ${this.price}$`
+	}
+}
+class AudiProductElectric implements ProductElectroCar {
+	public model(): string {
+		return "Audi A6"
+	}
+
+}
+
+class BmwProductDiesel implements ProductDieselCar {
+	price: number = 3000
+	model: string = "Bmw M5"
+	public info(): string {
+		return `Mодель авто ${this.model} его цена ${this.price}$`
 	}
 }
 
-class SportCar {
-	bmwM4() {
-		return new BmwM4();
+class BmwProductElectric implements ProductElectroCar {
+	public model(): string {
+		return "Bmw I3"
+	}
+}
+//====================================================================
+
+interface AbstractFactoryCar {     // абстрактная фабрика
+	createDieselCar(): ProductDieselCar;
+	createElectroCar(): ProductElectroCar;
+}
+
+class FactoryAudiCar implements AbstractFactoryCar {    //фабрика по производству ауди
+	public createDieselCar(): ProductDieselCar {
+		return new AudiProductDiesel();
+	}
+	public createElectroCar(): ProductElectroCar {
+		return new AudiProductElectric();
 	}
 }
 
-class FamilyCar {
-	bmwX5() {
-		return new BmwX5();
+class FactoryBmwCar implements AbstractFactoryCar { // такая же фабрика только BMW
+	public createDieselCar(): ProductDieselCar {
+		return new BmwProductDiesel();
 	}
-}
-
-class BmwM4 {
-	info() {
-		return 'Bmw sport car'
-	}
-}
-class BmwX5 {
-	info() {
-		return 'Bmw family car'
+	public createElectroCar(): ProductElectroCar {
+		return new BmwProductElectric();
 	}
 }
 
 
-let carTest1 = new ProducerCar()
-let carTest2 = carTest1.create('sport');
-let carTest3 = carTest1.create('family');
-console.log(carTest2)
-console.log(carTest3)
+function code(factory: AbstractFactoryCar) {
+	const audi = factory.createDieselCar();
+	const bmw = factory.createElectroCar();
+	console.log(audi.info());
+	console.log(bmw.model());
+}
+
+code(new FactoryBmwCar())
+code(new FactoryAudiCar());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// type CarsType = string | number;
+
+// interface Producer {
+// 	create(typeCar: CarsType): CarsType
+// }
+
+// class ProducerCar implements Producer {          // абстрактная фабрика
+// 	create(typeCar: CarsType): CarsType {
+// 		if (typeCar === 'family')
+// 			return new FamilyCar().bmwX5()
+// 		if (typeCar === 'sport')
+// 			return new SportCar().bmwM4()
+// 	}
+
+// }
+
+// class SportCar {
+// 	bmwM4(): CarsType {
+// 		return new BmwM4().info();
+// 	}
+// }
+
+// class FamilyCar {
+// 	bmwX5(): CarsType {
+// 		return new BmwX5().info();
+// 	}
+// }
+
+// class BmwM4 {
+// 	info(): CarsType {
+// 		return 'Bmw sport car'
+// 	}
+// }
+// class BmwX5 {
+// 	info(): CarsType {
+// 		return 'Bmw family car'
+// 	}
+// }
+
+// let carTest1 = new ProducerCar()
+// let carTest2 = carTest1.create('sport');
+// let carTest3 = carTest1.create('family');
+// console.log(carTest2)
+// console.log(carTest3)
 
 
 
